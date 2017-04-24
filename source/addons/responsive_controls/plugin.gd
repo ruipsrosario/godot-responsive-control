fee068254d0ab1c6536181a4b70b9e1512fe06da
Rui Rosário <_rpsr@sapo.pt>
2017-04-24 03:06:10 +0100

    Add responsive sizing placeholder type

--- source/addons/responsive_controls/plugin.gd
+++ source/addons/responsive_controls/plugin.gd
@@ -3,6 +3,8 @@ extends EditorPlugin
 
 func _enter_tree():
 	add_custom_type("ResponsiveSection", "Control", preload("ResponsiveSection.gd"), preload("icon_responsive_section.tex"))
+	add_custom_type("ResponsiveSizing", "Control", preload("ResponsiveSizing.gd"), preload("icon_responsive_sizing.tex"))
 
 func _exit_tree():
+	remove_custom_type("ResponsiveSizing")
 	remove_custom_type("ResponsiveSection")
