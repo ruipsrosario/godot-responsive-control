697f572c550f557dc8f47e2b6c06d47b6c728c9a
Rui Rosário <_rpsr@sapo.pt>
2017-04-24 03:18:52 +0100

    Add responsive sizing implementation

--- source/addons/responsive_controls/ResponsiveSizing.gd
+++ source/addons/responsive_controls/ResponsiveSizing.gd
@@ -1,5 +1,21 @@
 tool
 extends Control
 
-func _ready():
-	pass
+# Minimum size the parent Control must have in order to trigger this Responsive Sizing
+export(Vector2) var minimumParentSize
+
+# Whether or not this Responsive Sizing is visible in the set size
+export(bool) var isVisible
+
+# Checks if this Responsive Sizing is eligible under the specified size
+func isEligible(size):
+	return size.width >= minimumParentSize.width && size.height >= minimumParentSize.height
+
+# Applies this Responsive Sizing to the specified Control
+func applyTo(control):
+	control.set_h_size_flags(get_h_size_flags())
+	control.set_v_size_flags(get_v_size_flags())
+	control.set_scale(get_scale())
+	for margin in [ MARGIN_BOTTOM, MARGIN_TOP, MARGIN_RIGHT, MARGIN_LEFT ]:
+		control.set_anchor(margin, get_anchor(margin))
+		control.set_margin(margin, get_marginr(margin))
