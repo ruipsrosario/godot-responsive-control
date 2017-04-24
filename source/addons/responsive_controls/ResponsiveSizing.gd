406171d901f4f72d0027760f586ca05302feb0d9
Rui Rosário <_rpsr@sapo.pt>
2017-04-24 03:07:30 +0100

    Add missing tool keyword

--- source/addons/responsive_controls/ResponsiveSizing.gd
+++ source/addons/responsive_controls/ResponsiveSizing.gd
@@ -1,10 +1,5 @@
-extends Node
-
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+tool
+extends Control
 
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
 	pass
